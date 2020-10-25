51f4eae584f4b0c668cce0f4f50a28a2d2f6be54
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-10-25 15:28:47 -0400

    Created function to retrieve world covid data

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const covidInfoSearchUrl = 'https://disease.sh/v3/covid-19/jhucsse';
+'use strict';
+const covidInfoSearchUrl = 'https://disease.sh/v3/covid-19';
 
 const googleMapInfoSearchUrl = "";
+
+function displayResults(responseJson){
+    const cases = responseJson.cases;
+    const deaths = responseJson.deaths;
+    const recovered = responseJson.recovered;
+    $('.Cases').empty();
+    $('.Cases').append(`Cases: ${cases}`)
+    $('.Deaths').empty();
+    $('.Deaths').append(`Deaths: ${deaths}`)
+    $('.Recovered').empty();
+    $('.Recovered').append(`Recovered: ${recovered}`)
+}
+
+function getCurrentWorldData(){
+    const url = `${covidInfoSearchUrl}/all?yesterday=false&twoDaysAgo=false&allowNull=true`
+    fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+$(getCurrentWorldData())
